6f21618c48146efbcbd4ecdde23e0a75ea26e054
Haridarshan <haridarshan.choudhary@idfy.com>
2023-01-14 10:04:30 +0530

    feat(initial commit): initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
-    <p>
+    <p className="bg-red-400" >
       <Link href="/about">About</Link>
     </p>
   </Layout>
